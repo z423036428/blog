659016cd7b59c23fe520d8543c0471c00ab53b2a
playseebar <1142465631@qq.com>
2020-05-22 10:21:05 +0800

    注册页面，登录注册和详情跳转功能。 未实现业务逻辑。

--- src/main/java/com/blog/blog/controller/AdminController.java
+++ src/main/java/com/blog/blog/controller/AdminController.java
@@ -1,9 +1,10 @@
 package com.blog.blog.controller;
 
+import com.blog.blog.module.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping(value = "/admin")
 @Controller
@@ -14,8 +15,28 @@ public class AdminController {
         return "/admin/login.html";
     }
 
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public String register(){
+        return "/admin/register.html";
+    }
+
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
         return "/admin/index.html";
     }
+
+    //登录
+    @PostMapping("/userLogin")
+    public String doLogin(@RequestParam String username, @RequestParam String password){
+        System.out.println("返回值是》》"+username+"---->"+password);
+        return "/admin/index.html";
+    }
+
+    //注册
+    @PostMapping("/userRegister")
+    public String doRegister(@RequestParam String username, @RequestParam String password1, @RequestParam String password2){
+        System.out.println("返回值是》》"+username+"---->"+password1+"---->"+password2);
+        return "/admin/login.html";
+    }
+
 }
